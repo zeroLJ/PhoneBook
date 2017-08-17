cecbe39ecb1b2f5702105bb6024abb0530953f13
zeroLJ <940034240@qq.com>
2017-08-17 13:45:48 +0800

    添加通讯录模糊搜索联系人功能

--- PhoneBookDemo/app/src/main/java/com/zero/phonebookdemo/Fragment/CallListFragment.java
+++ PhoneBookDemo/app/src/main/java/com/zero/phonebookdemo/Fragment/CallListFragment.java
@@ -10,10 +10,14 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 import com.bigkoo.quicksidebar.QuickSideBarTipsView;
 import com.bigkoo.quicksidebar.QuickSideBarView;
@@ -25,26 +29,70 @@ import com.zero.phonebookdemo.R;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import static android.content.Context.INPUT_METHOD_SERVICE;
 
 /**
  *create by zero on 2017/8/10.
  */
 public class CallListFragment extends Fragment {
-    private ArrayList<ContactInfo> contactInfoList = new ArrayList<>();
+    //侧边字母滑动栏要显示的文字
     private String[] letters = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","#"};
+
+    //通讯录所有联系人列表相关
+    private ArrayList<ContactInfo> contactInfoList = new ArrayList<>();
     private ArrayList<ContactInfo>[] letterList = new ArrayList[letters.length];
-    HashMap<String,Integer> hashMap = new HashMap<>();//存储每个字母在列表中的位置
+    private HashMap<String,Integer> hashMap = new HashMap<>();
     private CallListAdapter adapter;
+
+    //搜索结果的联系人列表相关
+    private ArrayList<ContactInfo> contactInfoList_search = new ArrayList<>();
+    private ArrayList<ContactInfo>[] letterList_search;
+    private HashMap<String,Integer> hashMap_search = new HashMap<>();
+    private CallListAdapter adapter_search;
+
+
     private RecyclerView recyclerView;
-    LinearLayoutManager manager;
-    QuickSideBarView quickSideBarView;
-    QuickSideBarTipsView quickSideBarTipsView;
+    private LinearLayoutManager manager;
+    private QuickSideBarView quickSideBarView;
+    private QuickSideBarTipsView quickSideBarTipsView;
+    private EditText search_et;
+
+    public static final int INSERT = 1001;
+    public static final int SEARCH = 1002;
+    public static final int SEARCHFINISH = 1003;
+    public static final int NORMAL = 1004;
+
+    private InputMethodManager imm;
+
+
+    private Thread thread;
+    private Timer timer;
+
+    private boolean isSearching = false;
     private Handler handler = new Handler(){
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
-            adapter.notifyItemChanged(contactInfoList.size()-1);
-//            adapter.notifyDataSetChanged();
+            switch (msg.what){
+                case INSERT:
+                    adapter.notifyItemChanged(contactInfoList.size()-1);
+                    break;
+                case SEARCH:
+                    Log.i("ssss",msg.obj.toString());
+                    String str = msg.obj.toString();
+                    getSearchCallList(str);
+                    break;
+                case SEARCHFINISH:
+                    adapter_search = new CallListAdapter(getContext(), contactInfoList_search);
+                    recyclerView.setAdapter(adapter_search);
+                    break;
+                case NORMAL:
+                    recyclerView.setAdapter(adapter);
+                    break;
+            }
         }
     };
 
@@ -60,9 +108,22 @@ public class CallListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        imm = (InputMethodManager) getActivity().getSystemService(INPUT_METHOD_SERVICE);
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_call_list, container, false);
+        initView(view);
+        setListener();
+
+        return view;
+    }
+
+
+
+    private void initView(View view) {
+        quickSideBarView = (QuickSideBarView) view.findViewById(R.id.quickSideBarView);
+        quickSideBarTipsView = (QuickSideBarTipsView) view.findViewById(R.id.quickSideBarTipsView);
         recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView_callList);
+        search_et = (EditText) view.findViewById(R.id.search_et_callList);
         manager =new LinearLayoutManager(getContext());
         recyclerView.setLayoutManager(manager);
         //设置Item增加、移除动画
@@ -72,8 +133,15 @@ public class CallListFragment extends Fragment {
         recyclerView.setAdapter(adapter);
         getCallList();
 
-        quickSideBarView = (QuickSideBarView) view.findViewById(R.id.quickSideBarView);
-        quickSideBarTipsView = (QuickSideBarTipsView) view.findViewById(R.id.quickSideBarTipsView);
+        ArrayList<String> arrayList =new ArrayList<>();
+        for (int i=0;i<letters.length;i++){
+            arrayList.add(letters[i]);
+        }
+        //不自定义则默认26个字母
+        quickSideBarView.setLetters(arrayList);
+    }
+
+    private void setListener() {
         //设置监听
         quickSideBarView.setOnQuickSideBarTouchListener(new OnQuickSideBarTouchListener() {
             @Override
@@ -83,27 +151,69 @@ public class CallListFragment extends Fragment {
 //                if(letters.containsKey(letter)) {
 //                    recyclerView.scrollToPosition(letters.get(letter));
 //                }
-                if(hashMap.containsKey(letter)){
-                    //滑动到对应字母所在的位置，并在屏幕的顶部
-                    manager.scrollToPositionWithOffset(hashMap.get(letter),0);
-                    manager.setStackFromEnd(true);
+                if(isSearching){
+                    if(hashMap_search.containsKey(letter)){
+                        manager.scrollToPositionWithOffset(hashMap_search.get(letter),0);
+                        manager.setStackFromEnd(true);
+                    }
+                }else {
+                    if(hashMap.containsKey(letter)){
+                        manager.scrollToPositionWithOffset(hashMap.get(letter),0);
+                        manager.setStackFromEnd(true);
+                    }
                 }
+
             }
 
             @Override
             public void onLetterTouching(boolean touching) {
+                imm.hideSoftInputFromWindow(search_et.getWindowToken(), 0);
                 //可以自己加入动画效果渐显渐隐
                 quickSideBarTipsView.setVisibility(touching? View.VISIBLE: View.INVISIBLE);
             }
         });
 
-        ArrayList<String> arrayList =new ArrayList<>();
-        for (int i=0;i<letters.length;i++){
-            arrayList.add(letters[i]);
-        }
-        //不自定义则默认26个字母
-        quickSideBarView.setLetters(arrayList);
-        return view;
+        search_et.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (timer!=null){
+                    timer.cancel();
+                }
+                timer = new Timer();
+                timer.schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        String s =  search_et.getText().toString();
+                        if (!s.equals("")){
+                            handler.obtainMessage(CallListFragment.SEARCH, s).sendToTarget();
+                            isSearching = true;
+                        }else {
+                            handler.obtainMessage(NORMAL).sendToTarget();
+                            isSearching = false;
+                        }
+
+                    }
+                },500);
+            }
+        });
+
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                imm.hideSoftInputFromWindow(search_et.getWindowToken(), 0);
+            }
+        });
     }
 
     public static CallListFragment newInstance() {
@@ -149,23 +259,82 @@ public class CallListFragment extends Fragment {
                     }
                 }
 
-                //为每个字母作为一个item添加进adapter里面，并记录其位置
                 for (int i = 0; i < letters.length; i++) {
                         hashMap.put(letters[i],contactInfoList.size());
-                        Log.i("ssss","s"+contactInfoList.size());
                     if(letterList[i]!=null){
                         contactInfoList.add(new ContactInfo(letters[i]));
-                        handler.sendEmptyMessage(0);
+                        handler.obtainMessage(INSERT).sendToTarget();
                         for (int j = 0 ;j < letterList[i].size();j++){
                             contactInfoList.add(letterList[i].get(j));
-                            handler.sendEmptyMessage(0);
+                            handler.obtainMessage(INSERT).sendToTarget();
                         }
                     }
                 }
-                handler.sendEmptyMessage(0);
             }
         }).start();
     }
+
+    private void getSearchCallList(final String s) {
+        if (thread!=null){
+            handler.removeCallbacks(thread);
+        }
+        letterList_search = new ArrayList[letters.length];
+        hashMap_search.clear();
+        contactInfoList_search.clear();
+        //新开线程，防止通讯录太多联系人，卡顿
+        thread =  new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String[] cols = {ContactsContract.PhoneLookup.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.CONTACT_ID};
+                //设置查询条件
+                String selection = ContactsContract.PhoneLookup.DISPLAY_NAME + " like '%"+s+"%'";
+                //ContactsContract.CommonDataKinds.Phone.SORT_KEY_PRIMARY 这个参数是让查询结果按字母排序
+                Cursor cursor = getContext().getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                        cols, selection, null, ContactsContract.CommonDataKinds.Phone.SORT_KEY_PRIMARY);
+                if (cursor.getCount()>0){
+                    int nameFieldColumnIndex = cursor.getColumnIndex(ContactsContract.PhoneLookup.DISPLAY_NAME);
+                    int contactIdFieldColumnIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.CONTACT_ID);
+                    for (int i = 0; i < cursor.getCount(); i++) {
+                        cursor.moveToPosition(i);
+                        String name = cursor.getString(nameFieldColumnIndex);
+                        Long contactId = cursor.getLong(contactIdFieldColumnIndex);
+                        String letter = HanZiToPinYinUtil.getFirstLetter(name);
+                        ContactInfo contactInfo = new ContactInfo(name,contactId);
+                        boolean isLetter = false;
+                        for (int j = 0; j < letters.length; j++){
+                            if(letter.equals(letters[j])){
+                                if (letterList_search[j]==null){
+                                    letterList_search[j] = new ArrayList<ContactInfo>();
+                                }
+                                letterList_search[j].add(contactInfo);
+                                isLetter = true;
+                                break;
+                            }
+                        }
+                        if(!isLetter){
+                            if (letterList_search[26]==null){
+                                letterList_search[26] = new ArrayList<ContactInfo>();
+                            }
+                            letterList_search[26].add(contactInfo);
+                        }
+                    }
+
+                    for (int i = 0; i < letters.length; i++) {
+                        hashMap_search.put(letters[i],contactInfoList_search.size());
+                        if(letterList_search[i]!=null){
+                            contactInfoList_search.add(new ContactInfo(letters[i]));
+                            for (int j = 0 ;j < letterList_search[i].size();j++){
+                                contactInfoList_search.add(letterList_search[i].get(j));
+                            }
+                        }
+                    }
+                }
+                handler.obtainMessage(SEARCHFINISH).sendToTarget();
+            }
+        });
+        thread.start();
+    }
+
 
 }
 
